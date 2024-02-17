5a4bf32d6d56042a0fdb3c89aaa732a1356e9d72
Corentin Destrez <corentin.destrez62@gmail.com>
2024-02-17 22:57:43 +0100

    Backup PC portable

--- app-java/src/main/java/fr/lafierage/app_java/MainActivity.java
+++ app-java/src/main/java/fr/lafierage/app_java/MainActivity.java
@@ -2,7 +2,10 @@ package fr.lafierage.app_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button monBOuton =  findViewById(R.id.monSuperBouton);
+
+        monBOuton.setOnClickListener((truc) -> {
+            Intent intent = new Intent(this, MainActivity2.class);
+            startActivity(intent);
+        });
+
+
     }
 }
